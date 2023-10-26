40eaf0f8d4c05bb8cd25dd8819cd4b14c69336c2
Avvrk <jomaldonado480@gmail.com>
2023-10-26 03:09:18 -0500

    Agregandole los estilos y finalizar el funcionamiento de el programa.

--- main.js
+++ main.js
@@ -1,82 +1,100 @@
-let budgets = [];
-let bG = null;
+let presupuesto = 0;
+let gastos = [];
+let presupuestoRestante = 0;
 
-function validateExpenses() {
-    let exp = document.getElementById('gasto').value;
-    let quantity = document.getElementById('cantidad').value;
+function validarGastos() {
+    let gasto = document.getElementById('gasto').value;
+    let cantidad = parseFloat(document.getElementById('cantidad').value);
 
-    if (exp == '')
-        swal('Advertencia', 'Por favor llene el campo de "gasto".', 'warning');
-    else if (quantity == '')
-        swal('Advertencia', 'Por favor llene el campo de "cantidad".', 'warning');
-    else if (quantity < 0)
-        swal('Error', 'Por favor ingrese un valor mayor a 0', 'error');
-    else if (!isNaN(exp))
+    if (gasto == '') {
+        swal('Advertencia', 'Por favor llena el campo de "Gasto".', 'warning');
+    } else if (isNaN(cantidad) || cantidad <= 0) {
+        swal('Error', 'Por favor ingrese un valor válido en "Cantidad" (Número mayor a 0).', 'error');
+    } else if (!isNaN(gasto)) {
         swal('Error', 'Por favor ingrese un valor válido en "Gasto" (Solo letras).', 'error');
-    else if (isNaN(quantity))
-        swal('Error', 'Por favor ingrese un valor válido en "Cantidad" (Solo números).', 'error');
-    else {
-        swal('¡Exelente!', "Tu gasto a sido registrado", "success");
-        expense()
+    } else {
+        if (cantidad > presupuestoRestante) {
+            swal('Error', 'El gasto supera el presupuesto restante.', 'error');
+        } else {
+            swal('¡Excelente!', "Tu gasto ha sido registrado", "success");
+            registrarGasto(gasto, cantidad);
+            presupuestoRestante -= cantidad;
+            actualizarPresupuestoRestante();
+        }
     }
-
 }
 
-function validateBudget() {
-    let budget = document.getElementById('presupuesto').value;
+function validarPresupuesto() {
+    let presupuestoIngresado = parseFloat(document.getElementById('presupuesto').value);
 
-    if (budget == '')
-        swal('Advertencia', 'Por favor llena el campo de "Presupuesto".', 'warning');
-    else if (budget < 1)
-        swal('Error', 'Por favor ingrese un valor mayor a 0', 'error');
-    else 
-        bG = budget;
+    if (isNaN(presupuestoIngresado) || presupuestoIngresado <= 0) {
+        swal('Error', 'Por favor ingrese un valor válido en "Presupuesto" (Número mayor a 0).', 'error');
+    } else {
+        presupuesto = presupuestoIngresado;
+        presupuestoRestante = presupuestoIngresado;
+        actualizarPresupuestoRestante();
+    }
 }
 
-function expense() {
-    let exp = document.getElementById('gasto').value;
-    let quantity = document.getElementById('cantidad').value;
-
-    budgets.push({
-        gasto: exp,
-        cantidad: quantity,
-    })
+function registrarGasto(gasto, cantidad) {
+    gastos.push({
+        nombre: gasto,
+        cantidad: cantidad,
+    });
 
     document.getElementById("tarjeta").innerHTML = "";
 
-    showCard()
+    mostrarGastos();
 }
 
-function showCard() {
-    budgets.forEach((item, index) => {
-        let tr = document.createElement('tr');
-        let name = document.createElement('td');
-        let number = document.createElement('td');
-        let delet = document.createElement('td');
-        let dL = document.createElement('button');
+function mostrarGastos() {
+    gastos.forEach((item, index) => {
+        let fila = document.createElement('tr');
+        fila.classList.add('borde');
+        let nombre = document.createElement('td');
+        let cantidad = document.createElement('td');
+        let eliminar = document.createElement('td');
+        let botonEliminar = document.createElement('button');
 
-        dL.textContent = 'Borrar';
-        dL.addEventListener('click', () => {
-            eraseCard(index)
-        })
+        botonEliminar.textContent = 'Borrar';
+        botonEliminar.addEventListener('click', () => {
+            eliminarGasto(item, index);
+        });
 
-        name.textContent = item.gasto;
-        number.textContent = item.cantidad;
-        
-        delet.appendChild(dL);
-        tr.appendChild(name);
-        tr.appendChild(number);
-        tr.appendChild(delet);
+        nombre.textContent = item.nombre;
+        cantidad.textContent = item.cantidad;
 
-        document.getElementById('tarjeta').appendChild(tr);
-    })
+        eliminar.appendChild(botonEliminar);
+        fila.appendChild(nombre);
+        fila.appendChild(cantidad);
+        fila.appendChild(eliminar);
+
+        document.getElementById('tarjeta').appendChild(fila);
+
+        let filaVacia = document.createElement('tr');
+        let celdaVacia = document.createElement('td');
+
+        celdaVacia.setAttribute('colspan', '3');
+        filaVacia.appendChild(celdaVacia);
+
+        document.getElementById('tarjeta').appendChild(filaVacia);
+    });
 }
 
-function eraseCard(index) {
+function eliminarGasto(item, index) {
     let i = index;
-    budgets.splice(i, 1);
+    const cantidadADevolver = gastos[i].cantidad;
+    gastos.splice(i, 1);
 
     document.getElementById("tarjeta").innerHTML = "";
 
-    showCard();
+    mostrarGastos();
+
+
+    presupuestoRestante += cantidadADevolver;
+    actualizarPresupuestoRestante();
+}
+
+function actualizarPresupuestoRestante() {
+    document.getElementById('cambRestante').textContent = presupuestoRestante.toFixed(2);
 }
